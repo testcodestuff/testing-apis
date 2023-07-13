39355306ec023a396545c45a71f05164f4f961f8
testcodestuff <testingcodestuff@proton.me>
2023-07-12 23:07:43 -0300

    Add users router to main

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from routers import products, users
+from routers import products, users, basic_auth_users
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.include_router(products.router)
 app.include_router(users.router)
+app.include_router(basic_auth_users.router)
 app.mount("/static", StaticFiles(directory="static"), name="dog")
 
 
